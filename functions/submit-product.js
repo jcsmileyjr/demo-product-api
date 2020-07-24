362e2b970caa616ef8d5fb553a60a0de378ce95b
Smiley <jcsmileyjr@gmail.com>
2020-07-23 21:33:20 -0500

    Create a record API

---
+++ functions/submit-product.js
@@ -0,0 +1,32 @@
+var Airtable = require('airtable');
+const {key, baseId} = process.env; // Get the enviroment values defined in netlify site 
+var base = new Airtable({apiKey:key}).base(baseId);
+
+const table = base('sighting');
+
+const submitRecord = async (fields) => {
+    await table.create(fields);
+}
+
+exports.handler = function(event, context, callback) {
+    const {product} = JSON.parse(event.body);
+
+    const newRecord= {
+      'Category': product.Category,
+      'Type': product.Type,
+      'Description': product.Description,
+      'Location': product.Location,
+    }
+    
+    submitRecord(newRecord);
+    
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        /* Required for CORS support to work */
+        'Access-Control-Allow-Origin': '*',
+        /* Required for cookies, authorization headers with HTTPS */
+        'Access-Control-Allow-Credentials': true
+      },
+    });
+  };
